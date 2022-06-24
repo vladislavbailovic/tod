9615a08fddc34dbb100a6b9995c3b56046b26488
vladislav <ve@incsub.com>
2022-06-24 04:52:07 +0200

    Args load named and positional args

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -8,9 +8,25 @@ struct Args {
 
 impl From<Vec<String>> for Args {
     fn from(args: Vec<String>) -> Self {
+        let mut named = HashMap::new();
+        let mut positional = Vec::new();
+
+        let args = Self::normalize(args);
+        let mut args = args.iter();
+        while let Some(arg) = args.next() {
+            if arg.contains("-") {
+                let name = arg.clone();
+                if let Some(arg) = args.next() {
+                    named.insert(name, arg.clone());
+                }
+            } else {
+                positional.push(arg.clone());
+            }
+        }
+
         Self{
-            named: HashMap::new(),
-            positional: Self::normalize(args)
+            named: named,
+            positional: positional
         }
     }
 }
@@ -37,6 +53,21 @@ mod test {
     use super::*;
 
     #[test]
+    fn normalize_breaks_arg_on_equals() {
+        let result = Args::normalize(vec![ "-one=test".to_string() ]);
+        assert!(result.len() == 2, "unexpected len: {:?} [{}]", result, result.len());
+        assert!(result.contains(&"-one".to_string()));
+        assert!(result.contains(&"test".to_string()));
+    }
+
+    #[test]
+    fn normalize_requires_dashes_to_break() {
+        let result = Args::normalize(vec![ "one=test".to_string() ]);
+        assert!(result.len() == 1, "unexpected len: {:?} [{}]", result, result.len());
+        assert!(result.contains(&"one=test".to_string()));
+    }
+
+    #[test]
     fn recognizes_positional_args() {
         let args = Args::from(vec![ "one".to_string(), "two".to_string(), "three".to_string() ]);
         assert!(args.positional.len() == 3);
@@ -44,10 +75,11 @@ mod test {
     }
 
     #[test]
-    fn normalize_breaks_arg_on_equals() {
-        let result = Args::normalize(vec![ "-one=test".to_string() ]);
-        assert!(result.len() == 2, "unexpected len: {:?} [{}]", result, result.len());
-        assert!(result.contains(&"-one".to_string()));
-        assert!(result.contains(&"test".to_string()));
+    fn recognizes_named_args() {
+        let args = Args::from(vec!["-one=test".to_string(), "-two".to_string(), "value".to_string()]);
+        assert!(args.positional.len() == 0);
+        assert!(args.named.len() == 2);
+        assert!(args.named["-one"] == "test");
+        assert!(args.named["-two"] == "value");
     }
 }
