ca49e58fb3e0209c0cc5df9084bb0fbe1d3cf12b
vladislav <ve@incsub.com>
2022-06-28 03:36:05 +0200

    Use flag base when setting up flags

--- src/cli/subcommand.rs
+++ src/cli/subcommand.rs
@@ -4,6 +4,7 @@ struct Arguments<'cmd> {
     positional: Vec<&'cmd str>,
     named: HashMap<&'cmd str, &'cmd str>,
     boolean: Vec<&'cmd str>,
+    exact: Vec<&'cmd str>,
     args: Vec<&'cmd str>,
     supported: Vec<Flag<'cmd>>,
 }
@@ -15,6 +16,14 @@ struct Flag<'cmd> {
 }
 
 impl<'cmd> Flag<'cmd> {
+    fn flag_base(flag: &str) -> Option<&str> {
+        if flag.contains('-') {
+            Some(str::trim_start_matches(flag, '-'))
+        } else {
+            None
+        }
+    }
+
     fn full(&self) -> String {
         format!("--{}", self.name)
     }
@@ -32,28 +41,44 @@ enum FlagType {
 }
 
 impl<'cmd> Arguments<'cmd> {
-    fn new(supported: Vec<Flag<'cmd>>) -> Self {
+    pub fn new(supported: Vec<Flag<'cmd>>) -> Self {
         Self {
             supported,
             args: Vec::new(),
             positional: Vec::new(),
             boolean: Vec::new(),
+            exact: Vec::new(),
             named: HashMap::new(),
         }
     }
 
-    fn parse(&mut self, args: Vec<&'cmd str>) {
+    pub fn parse(&mut self, args: Vec<&'cmd str>) {
         self.args = args.clone();
         let args = self.parse_boolean(args);
         let args = self.parse_named(args);
+        let args = self.parse_exact(args);
         self.positional = args;
     }
 
-    fn subcommand(args: Vec<&'cmd str>) -> (Option<&'cmd str>, Vec<&'cmd str>) {
+    pub fn subcommand(args: Vec<&'cmd str>) -> (Option<&'cmd str>, Vec<&'cmd str>) {
         if args.len() < 2 {
             return (None, args);
         }
         (Some(args[1]), args[2..].to_vec())
+    }
+
+    pub fn has(&self, flag: Flag) -> bool {
+        match flag.kind {
+            FlagType::Boolean => self.boolean.contains(&flag.name),
+            FlagType::Exact => self.exact.contains(&flag.name),
+            FlagType::Value => {
+                if let Some(_) = self.named.get(flag.name) {
+                    true
+                } else {
+                    false
+                }
+            },
+        }
     }
 
     fn get_supported(&self, kind: FlagType) -> Vec<&'cmd str> {
@@ -67,14 +92,15 @@ impl<'cmd> Arguments<'cmd> {
     }
 
     fn parse_boolean(&mut self, args: Vec<&'cmd str>) -> Vec<&'cmd str> {
-        let supported: Vec<&'cmd str> = self.get_supported(FlagType::Boolean);
+        let boolean: Vec<&'cmd str> = self.get_supported(FlagType::Boolean);
         args.into_iter().filter(|x| {
-            if supported.contains(x) {
-                self.boolean.push(x);
-                false
-            } else {
-                true
+            if let Some(x) = Flag::flag_base(x) {
+                if boolean.contains(&x) {
+                    self.boolean.push(x);
+                    return false;
+                }
             }
+            true
         }).collect()
     }
 
@@ -83,29 +109,29 @@ impl<'cmd> Arguments<'cmd> {
         let mut remaining = Vec::new();
         let mut args = args.iter();
         while let Some(&arg) = args.next() {
-            if named.contains(&arg) {
-                if let Some(value) = args.next() {
-                    self.named.insert(arg, value);
-                    continue;
+            if let Some(arg) = Flag::flag_base(arg) {
+                if named.contains(&arg) {
+                    if let Some(value) = args.next() {
+                        self.named.insert(arg, value);
+                        continue;
+                    }
                 }
+            } else {
+                remaining.push(arg);
             }
-            remaining.push(arg);
         }
         remaining
     }
 
-    fn has(&self, flag: Flag) -> bool {
-        match flag.kind {
-            FlagType::Boolean => self.boolean.contains(&flag.name),
-            FlagType::Value => {
-                if let Some(_) = self.named.get(flag.name) {
-                    true
-                } else {
-                    false
-                }
-            },
-            FlagType::Exact => self.positional.len() > 0,
-        }
+    fn parse_exact(&mut self, args: Vec<&'cmd str>) -> Vec<&'cmd str> {
+        let exact: Vec<&'cmd str> = self.get_supported(FlagType::Exact);
+        args.into_iter().filter(|x| {
+            if exact.contains(&x) {
+                self.exact.push(x);
+                return false;
+            }
+            true
+        }).collect()
     }
 }
 
@@ -116,7 +142,7 @@ mod test {
     #[test]
     fn supported() {
         let args = Arguments::new(vec![
-            Flag{name: "--help", kind: FlagType::Boolean}
+            Flag{name: "help", kind: FlagType::Boolean}
         ]);
         let supported = args.get_supported(FlagType::Boolean);
         assert_eq!(supported.len(), 1);
@@ -125,11 +151,11 @@ mod test {
     #[test]
     fn boolean() {
         let mut args = Arguments::new(vec![
-            Flag{name: "--help", kind: FlagType::Boolean}
+            Flag{name: "help", kind: FlagType::Boolean}
         ]);
         let remaining = args.parse_boolean(vec!["one", "--help", "two"]);
         assert_eq!(args.boolean.len(), 1);
-        assert!(args.has(Flag{ name: "--help", kind: FlagType::Boolean}));
+        assert!(args.has(Flag{ name: "help", kind: FlagType::Boolean}));
 
         assert_eq!(remaining.len(), 2);
     }
@@ -137,13 +163,26 @@ mod test {
     #[test]
     fn named() {
         let mut args = Arguments::new(vec![
-            Flag{name: "--one", kind: FlagType::Value}
+            Flag{name: "one", kind: FlagType::Value}
         ]);
         let remaining = args.parse_named(vec!["--one", "two", "three"]);
         assert_eq!(args.named.len(), 1);
         assert_eq!(remaining.len(), 1);
 
-        assert_eq!(args.named["--one"], "two");
+        assert_eq!(args.named["one"], "two");
+    }
+
+    #[test]
+    fn exact() {
+        let mut args = Arguments::new(vec![
+            Flag{name: "help", kind: FlagType::Exact}
+        ]);
+        let remaining = args.parse_exact(vec!["one", "help", "--help", "two"]);
+        assert_eq!(args.exact.len(), 1);
+        assert!(args.has(Flag{ name: "help", kind: FlagType::Exact}));
+
+        assert_eq!(remaining.len(), 3);
+        assert!(remaining.contains(&"--help"));
     }
 
     #[test]
@@ -186,11 +225,11 @@ mod test {
 
         if let Some("ls") = subcommand {
             let mut supported = Arguments::new(vec![
-              Flag{ name: "--help", kind: FlagType::Boolean },
-              Flag{ name: "--dir", kind: FlagType::Value },
+              Flag{ name: "help", kind: FlagType::Boolean },
+              Flag{ name: "dir", kind: FlagType::Value },
             ]);
             supported.parse(args);
-            if let Some(&"../wat") = supported.named.get("--dir") {
+            if let Some(&"../wat") = supported.named.get("dir") {
                 assert!(true);
             } else {
                 assert!(false, "named flag not recognized");
@@ -206,5 +245,12 @@ mod test {
 
         assert_eq!("--help", &f.full());
         assert_eq!("-h", &f.short());
+
+        if let Some(flag) = Flag::flag_base("--help") {
+            assert_eq!("help", flag);
+        }
+        if let Some(flag) = Flag::flag_base("-h") {
+            assert_eq!("h", flag);
+        }
     }
 }
